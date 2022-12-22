519d9b436fd3a02fde40848b5a91e85274cdd3ee
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-22 19:39:03 +0300

    regular commit60

--- src/newjs.jsx
+++ src/newjs.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './newjs.css'
 const NewJs = () => {
   return (
     <>
-      <section><h1>Nicecurves</h1>
+      <section className='newone'><h1>Nicecurves</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
       <section><h1>Nicecurves</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
