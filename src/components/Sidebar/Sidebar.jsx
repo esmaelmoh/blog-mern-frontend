1f02017be45d62e5eff610687a0e7a764649280d
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-22 23:15:09 +0300

    regular commit220

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import './Sidebar.css'
 const Sidebar = () => {
   return (
-    <div>
-        
-    </div>
+    <section>
+        <div className="sidebar">
+            <div className="img-container">
+                <img src="" alt="" />
+            </div>
+            <div className="sidebar-menus">
+                <div className="menu-container">
+                    <h1>2</h1>
+                    <h3>Notification</h3>
+                </div>
+                <div className="menu-container">
+                    <h1>2</h1>
+                    <h3>Notification</h3>
+                </div>
+                <div className="menu-container">
+                    <h1>2</h1>
+                    <h3>Notification</h3>
+                </div>
+                <div className="menu-container">
+                    <h1>2</h1>
+                    <h3>Notification</h3>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
