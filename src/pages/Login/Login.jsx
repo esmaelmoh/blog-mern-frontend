281fb28ffee08b9ae0514f0952728aa3e147c8e5
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-22 21:08:58 +0300

    regular commit106

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,18 +10,19 @@ const Login = () => {
         <h1 data-aos="zoom-in-up" data-aos-duration="2000" >እንኳን ደህና መጡ</h1>
     </div> */}
     <div className="login-container-right">
-        <form action=""className='login-form'>
-            <h1 className='login-title'>Login</h1>
+    <section className='newone'>
+      <form action=""className='login-form-new'>
+            <h1 className='login-title-new'>Login</h1>
             <label htmlFor="">Username:</label>
-            <input type="text" name className='login-input-field'  id="" />
+            <input type="text" name className='login-input-field-new'  id="" />
             <label htmlFor="">Password:</label>
-            <input type="password" name className='login-input-field'  id="" />
+            <input type="password" name className='login-input-field-new'  id="" />
             <br /><br />
-            <button type="submit" className='login-btn'>Login</button>
+            <button type="submit" className='login-btn-new'>Login</button>
             <div className="line"></div>
-            <button type="submit" className='login-btn'>Sign In With Google  <GrGoogle/></button>
-            <p style={{color:'rgba(0, 0, 0, 0.705)',paddingTop:'0.7rem', textAlign:'center'}}>Don't have an account? <span style={{color:" #3963cb",fontWeight:'bold'}}>Sign Up Now!</span> </p>
-        </form>
+            <button type="submit" className='login-btn-new'>Sign In With Google  </button>
+            <p style={{color:'white',paddingTop:'0.7rem', textAlign:'center'}}>Don't have an account? <span style={{color:'#00e0ff',fontWeight:'bold'}}>Sign Up Now!</span> </p>
+        </form></section>  
     </div>
     </div>
   )
