814af481ef54f6cc23608151e53a2747372ad98e
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-22 19:24:00 +0300

    regular commit46

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div >
         {/* <Login/> */}
       <NewJs/>
     </div>
---
+++ src/newjs.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const NewJs = () => {
+  return (
+    <>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+      <section><h1>Nicecurves</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, porro! Molestiae quos dignissimos cupiditate ipsum autem, maiores corrupti eius aliquid eligendi enim voluptate voluptatum aut qui quis necessitatibus sed. Placeat?</p></section>
+        
+    </>
+  )
+}
+
+export default NewJs
