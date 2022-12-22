a835f8419ef30d99446378c26f38cab6d146f2dc
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-22 19:58:26 +0300

    regular commit78

--- src/newjs.jsx
+++ src/newjs.jsx
@@ -5,15 +5,15 @@ const NewJs = () => {
     <>
       <section className='newone'>
       <form action=""className='login-form-new'>
-            <h1 className='login-title'>Login</h1>
+            <h1 className='login-title-new'>Login</h1>
             <label htmlFor="">Username:</label>
-            <input type="text" name className='login-input-field'  id="" />
+            <input type="text" name className='login-input-field-new'  id="" />
             <label htmlFor="">Password:</label>
-            <input type="password" name className='login-input-field'  id="" />
+            <input type="password" name className='login-input-field-new'  id="" />
             <br /><br />
-            <button type="submit" className='login-btn'>Login</button>
+            <button type="submit" className='login-btn-new'>Login</button>
             <div className="line"></div>
-            <button type="submit" className='login-btn'>Sign In With Google  </button>
+            <button type="submit" className='login-btn-new'>Sign In With Google  </button>
             <p style={{color:'rgba(0, 0, 0, 0.705)',paddingTop:'0.7rem', textAlign:'center'}}>Don't have an account? <span style={{color:" #3963cb",fontWeight:'bold'}}>Sign Up Now!</span> </p>
         </form></section>  
     </>
