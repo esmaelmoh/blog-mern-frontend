49222d22f10737c35be792a782aa175f2855cd62
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-22 19:19:41 +0300

    regular commit42

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import NewJs from './newjs';
 import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-        <Login/>
-
+        {/* <Login/> */}
+      <NewJs/>
     </div>
   );
 }
