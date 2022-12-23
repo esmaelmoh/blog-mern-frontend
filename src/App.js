7ee28b5b7d14c6c378d6618d4f8fc2834ba9afc5
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-23 09:00:17 +0300

    regular commit281

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
+import {Route,Routes} from 'react-router-dom'
+
 function App() {
   return (
     < >
-
-      <Register/>
-      <Login/>
+     < Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+     </Routes>
+      {/* <Register/>
+      <Login/> */}
       {/* <Home/> */}
     </>
   );
