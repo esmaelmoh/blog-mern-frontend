5b6e80c3b28822271f3c5195b27c02f2c8c81bbf
esmaelmoh <esmaelmoh0132@gmail.com>
2022-12-23 08:46:16 +0300

    regular commit268

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
   const [email,setEmail]= useState('')
   console.log(username,password,email)
 
-  const handleChange = async(e)=>{
+  const handleSubmit = async(e)=>{
     e.preventDefault()
     try {
       const user = await Axios.post('http://localhost:5000/api/auth/register',{username,password,email})
@@ -21,7 +21,7 @@ const Register = () => {
         <div className='register-container'>
           <div className="register-container-right">
           <section className='newone'>
-            <form action=""className='register-form-new'>
+            <form action=""className='register-form-new' onSubmit={handleSubmit}>
                   <h1 className='register-title-new'>Register</h1>
                   <label htmlFor="">Username:</label>
                   <input type="text" name className='register-input-field-new' onChange={(e)=>setUsername(e.target.value)} id="" />
@@ -30,7 +30,7 @@ const Register = () => {
                   <label htmlFor="">Email Adress:</label>
                   <input type="email" name className='register-input-field-new' onChange={(e)=>setEmail(e.target.value)}  id="" />
                   <br /><br />
-                  <button type="submit" className='register-btn-new' onChange={handleChange}>Register</button>
+                  <button type="submit" className='register-btn-new' >Register</button>
                   <div className="line"></div>
                   <button type="submit" className='register-btn-new'>Sign In With Google  </button>
                   <p style={{color:'white',paddingTop:'0.7rem', textAlign:'center'}}>Don't have an account? <span style={{color:'#00e0ff',fontWeight:'bold'}}>Sign Up Now!</span> </p>
